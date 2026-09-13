a9e58668ee227d03cc692f53ee4e282361e804bb
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-09-13 21:27:27 +0530

    Update __init__.py

--- backend/infrastructure/ipam/__init__.py
+++ backend/infrastructure/ipam/__init__.py
@@ -1,11 +1,14 @@
-"""IP Address Management infrastructure for MAIN BASE FOUNDATION."""
+"""
+MAIN BASE FOUNDATION
+Infrastructure - IPAM Package
+"""
 
 from .model import IPAddressInfo
-from .service import IPAddressService
-from .controller import IPAddressController
+from .service import IPAMService
+from .controller import IPAMController
 
 __all__ = [
     "IPAddressInfo",
-    "IPAddressService",
-    "IPAddressController",
+    "IPAMService",
+    "IPAMController",
 ]
