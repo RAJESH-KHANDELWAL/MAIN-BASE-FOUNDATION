9a05382d128a72209db8f0ad59c6a3be60581a49
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-09-15 08:34:38 +0530

    Update __init__.py

--- backend/supreme/ecosystem/mukti_mahal/__init__.py
+++ backend/supreme/ecosystem/mukti_mahal/__init__.py
@@ -1,20 +1,32 @@
-self._mahals: Dict[str, MuktiMahal] = {}
-self._groups: Dict[str, PratapGroup] = {}
-self._family_members: Dict[
-    str, MuktiMahalFamilyMember
-] = {}
-self._staff: Dict[
-    str, MuktiMahalStaffMember
-] = {}
-self._estate_areas: Dict[
-    str, MuktiMahalEstateArea
-] = {}
-self._capability_evaluations: Dict[
-    str, BusinessCapabilityEvaluation
-] = {}
-self._family_visits: Dict[
-    str, MuktiMahalFamilyVisit
-] = {}
-self._settings: Dict[
-    str, MuktiMahalSetting
-] = {}
+"""
+MUKTI MAHAL
+
+Supreme Ecosystem -> Mukti Mahal
+"""
+
+from .model import (
+    MuktiMahal,
+    MuktiMahalFamilyMember,
+    MuktiMahalStaffMember,
+    MuktiMahalEstateArea,
+    PratapGroup,
+    BusinessCapabilityEvaluation,
+    MuktiMahalFamilyVisit,
+    MuktiMahalSetting,
+)
+
+from .service import MuktiMahalService
+from .repository import MuktiMahalRepository
+
+__all__ = [
+    "MuktiMahal",
+    "MuktiMahalFamilyMember",
+    "MuktiMahalStaffMember",
+    "MuktiMahalEstateArea",
+    "PratapGroup",
+    "BusinessCapabilityEvaluation",
+    "MuktiMahalFamilyVisit",
+    "MuktiMahalSetting",
+    "MuktiMahalService",
+    "MuktiMahalRepository",
+]
