172830831b51032f288e1212f475136efb90ce28
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-09-15 10:26:37 +0530

    Expose GenerationRequest and services in __init__.py

--- backend/creation/engines/__init__.py
+++ backend/creation/engines/__init__.py
@@ -1,1 +1,9 @@
+from .model import GenerationRequest
+from .service import GenerationEngineService
+from .controller import GenerationEngineController
 
+__all__ = [
+    "GenerationRequest",
+    "GenerationEngineService",
+    "GenerationEngineController",
+]
